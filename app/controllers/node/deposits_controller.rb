76f9e4f5a86e31c32fbb1dbc93e6bb81c16724ec
孟庆巍 <keven@lonzh.com>
2021-10-08 21:01:04 +0800

    init

--- app/controllers/node/deposits_controller.rb
+++ app/controllers/node/deposits_controller.rb
@@ -6,27 +6,27 @@ class Node::DepositsController < ApplicationController
       addr = item[:from].downcase
       if (user = User.find_by_address(addr)) && !Deposit.exists?(tx_hash: item[:tx_hash])
         amount = BigDecimal(item[:amount])
-        if item[:asset_type] == 'usdt'
-          User.where(id: user.id).update_all(['packet_usdt_available = packet_usdt_available + ?', amount])
-        elsif item[:asset_type] == 'cigar'
-          User.where(id: user.id).update_all(['candy_available = candy_available + ?', amount])
-        else
-          next
+        user.with_lock do
+          if item[:asset_type] == 'usdt'
+            User.where(id: user.id).update_all(['packet_usdt_available = packet_usdt_available + ?', amount])
+          else
+            User.where(id: user.id).update_all(['candy_available = candy_available + ?', amount])
+          end
+          Deposit.create(
+            user_id: user.id,
+            tx_hash: item[:tx_hash],
+            asset_type: item[:asset_type],
+            amount: amount,
+            address: addr
+          )
+          AssetFlow.create(
+            user_id: user.id,
+            account_type: :packet,
+            asset_type: item[:asset_type],
+            flow_type: :deposit,
+            amount: amount
+          )
         end
-        Deposit.create(
-          user_id: user.id,
-          tx_hash: item[:tx_hash],
-          asset_type: item[:asset_type],
-          amount: amount,
-          address: addr
-        )
-        AssetFlow.create(
-          user_id: user.id,
-          account_type: :packet,
-          asset_type: item[:asset_type],
-          flow_type: :deposit,
-          amount: amount
-        )
       end
     end
     success
