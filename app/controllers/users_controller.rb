193836e2f9ef7f5dd4cbfc16a240078a9756737d
孟庆巍 <keven@lonzh.com>
2021-10-11 16:35:34 +0800

    init

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,7 +74,7 @@ class UsersController < BaseUserController
       candy: cur_user.candy,
       candy_display: LZUtils.format_coin(cur_user.candy),
       cigar_usdt_price: config.cigar_usdt_price,
-      estimate_to_usdt: LZUtils.format_coin(cur_user.packet_usdt + cur_user.chain_usdt_available + (cur_user.chain_cigar_available + cur_user.candy) * config.cigar_usdt_price),
+      estimate_to_usdt: cur_user.packet_usdt + cur_user.chain_usdt_available + (cur_user.chain_cigar_available + cur_user.candy) * config.cigar_usdt_price,
       team_usdt_flow: cur_user.team_usdt_flow,
       team_usdt_flow_display: LZUtils.format_coin(cur_user.team_usdt_flow),
       invite_url: "#{Utils::Constants::BASE_URL}users/new?pid=#{cur_user.invite_code}"
