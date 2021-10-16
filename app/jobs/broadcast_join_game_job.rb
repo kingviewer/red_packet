907636b526fa37405d755512b3376f2aeffe6015
孟庆巍 <keven@lonzh.com>
2021-10-16 16:35:00 +0800

    init

--- app/jobs/broadcast_join_game_job.rb
+++ app/jobs/broadcast_join_game_job.rb
@@ -1,14 +1,14 @@
 class BroadcastJoinGameJob < ApplicationJob
   queue_as :default
 
-  def perform(game, user)
+  def perform(game, waiter_amount, user)
     ActionCable.server.broadcast(
       GamesChannel.channel_name(game.id),
       action: :join,
       id: game.id,
       times: game.times,
       user_id: user.id,
-      waiter_amount: game.waiter_amount,
+      waiter_amount: waiter_amount,
       player_amount: game.player_amount,
       progress: (game.waiter_amount * 100 / game.player_amount).to_i,
       invite_code: user.invite_code,
