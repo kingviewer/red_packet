b29ef6c9acef77b981616f8da87d7b536499c5b4
孟庆巍 <keven@lonzh.com>
2021-11-15 17:20:45 +0800

    init

--- app/controllers/user_soldiers_controller.rb
+++ app/controllers/user_soldiers_controller.rb
@@ -18,4 +18,19 @@ class UserSoldiersController < BaseUserController
     end
     success(data)
   end
+
+  def start_working
+    if not (us = UserSoldier.find_by(id: params[:id], user_id: cur_user.id, expired: false))
+      error(t('soldiers.buy.soldier_not_exist'))
+    else
+      us.with_lock do
+        if us.working?
+          error(t('soldiers.dispatch.soldier_already_working'))
+        else
+          us.update(start_working_at: Time.now, state: :working)
+          success
+        end
+      end
+    end
+  end
 end
