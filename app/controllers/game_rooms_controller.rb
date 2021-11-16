f5b688b81598e896d568fb2c3b2bd5871c64f759
孟庆巍 <keven@lonzh.com>
2021-11-16 20:28:22 +0800

    init

--- app/controllers/game_rooms_controller.rb
+++ app/controllers/game_rooms_controller.rb
@@ -10,9 +10,12 @@ class GameRoomsController < BaseUserController
 
   # 创建
   def create
-    game_room = GameRoom.new(params.require(:game_room).permit(:usdt_amount, :player_amount, :min_usdt_amount, :password, :loser_amount))
+    game_room = GameRoom.new(params.permit(:usdt_amount, :player_amount, :min_usdt_amount, :password))
+    game_room.loser_amount = game_room.player_amount - params[:winner_amount].to_i
     if game_room.usdt_amount <= 0 || game_room.player_amount <= 0 || game_room.loser_amount <= 0
       invalid_params
+    elsif game_room.usdt_amount % 10 != 0
+      error(t('.cic_amount_invalid'))
     elsif game_room.min_usdt_amount < game_room.usdt_amount
       error(t('.min_usdt_amount_less_than_game_required'))
     elsif game_room.loser_amount >= game_room.player_amount || game_room.loser_amount < 1
@@ -24,11 +27,11 @@ class GameRoomsController < BaseUserController
             error('.in_room_not_to_create')
           else
             User.where(id: cur_user.id).update_all(
-              ['packet_usdt_available = packet_usdt_available - ?, packet_usdt_frozen = packet_usdt_frozen + ?',
+              ['candy_available = candy_available - ?, candy_frozen = candy_frozen + ?',
                game_room.usdt_amount, game_room.usdt_amount]
             )
             cur_user.reload
-            if cur_user.packet_usdt_available < game_room.min_usdt_amount
+            if cur_user.candy_available < game_room.min_usdt_amount
               raise t('.usdt_available_less_than_min')
             else
               game_room.user_id = cur_user.id
@@ -54,7 +57,6 @@ class GameRoomsController < BaseUserController
   def query
     if (game_room = GameRoom.playing.find_by(id: params[:id]))
       ur = UserRoom.find_by(game_room_id: game_room.id, user_id: cur_user.id)
-      user_amount = UserRoom.where(game_room_id: game_room.id).count
       joiner_amount = UserRoom.where(game_room_id: game_room.id, joined: true).count
       success(
         id: game_room.id,
@@ -63,11 +65,9 @@ class GameRoomsController < BaseUserController
         player_amount: game_room.player_amount,
         loser_amount: game_room.loser_amount,
         min_usdt_amount: game_room.min_usdt_amount,
-        user_amount: user_amount,
         joiner_amount: joiner_amount,
         progress: (joiner_amount * 100 / game_room.player_amount).to_i,
         player_amount_display: "#{joiner_amount}/#{game_room.player_amount} #{t('user_game_rounds.index.person_number')}",
-        entered: !ur.nil?,
         joined: !ur.nil? && ur.joined
       )
     else
@@ -82,7 +82,7 @@ class GameRoomsController < BaseUserController
     elsif cur_user.packet_usdt_available < game_room.min_usdt_amount
       error(t('.join.usdt_available_insufficient'))
     else
-      success
+      success(id: game_room.id)
     end
   end
 
@@ -109,23 +109,22 @@ class GameRoomsController < BaseUserController
   def join
     if not (game_room = GameRoom.playing.find_by(id: params[:id]))
       error(t('.query.room_not_exist'))
-    elsif not (user_room = UserRoom.find_by(user_id: cur_user.id, game_room_id: game_room.id))
-      invalid_request
+    elsif UserRoom.exists?(user_id: cur_user.id, game_room_id: game_room.id)
+      error(t('.have_joined_the_room'))
     else
       game_room.with_lock do
-        user_room.reload
         if game_room.stopped?
           error(t('.room_stopped'))
-        elsif user_room.joined
+        elsif UserRoom.exists?(user_id: cur_user.id, game_room_id: game_room.id)
           error(t('.have_joined_the_room'))
         else
+          user_room = UserRoom.create(user_id: cur_user.id, game_room_id: game_room.id, joined: true)
           User.where(id: cur_user.id).update_all(
-            ['packet_usdt_available = packet_usdt_available - ?, packet_usdt_frozen = packet_usdt_frozen + ?',
+            ['candy_available = candy_available - ?, candy_frozen = candy_frozen + ?',
              game_room.usdt_amount, game_room.usdt_amount]
           )
           cur_user.reload
-          raise t('.usdt_available_insufficient') if cur_user.packet_usdt_available < 0
-          user_room.update(joined: true)
+          raise t('.cic_available_insufficient') if cur_user.packet_usdt_available < 0
           BroadcastJoinRoomJob.perform_later(game_room, cur_user)
           game_round_id = game_room.check_win
           success(win: !game_round_id.nil?, time: user_room.formatted_updated_at, game_round_id: game_round_id)
@@ -147,7 +146,7 @@ class GameRoomsController < BaseUserController
           success
         else
           User.where(id: cur_user.id).update_all(
-            ['packet_usdt_available = packet_usdt_available + ?, packet_usdt_frozen = packet_usdt_frozen - ?',
+            ['candy_available = candy_available + ?, candy_frozen = candy_frozen - ?',
              game_room.usdt_amount, game_room.usdt_amount]
           ) if user_room.joined
           user_room.destroy
@@ -167,7 +166,6 @@ class GameRoomsController < BaseUserController
     UserRoom.includes(:user).where(game_room_id: params[:id]).order(joined: :desc).order(id: :desc).each do |ur|
       data << {
         invite_code: ur.user.invite_code,
-        joined: ur.joined,
         created_at: ur.formatted_updated_at
       }
     end
