a5594b4f51dd81423e5976a79df0e46adf16b8bb
孟庆巍 <keven@lonzh.com>
2021-12-17 19:13:33 +0800

    init

--- app/controllers/user_soldiers_controller.rb
+++ app/controllers/user_soldiers_controller.rb
@@ -38,7 +38,7 @@ class UserSoldiersController < BaseUserController
           error(t('.today_has_worked'))
         else
           us.update(start_working_at: Time.now, state: :working)
-          WorkingRecord.create(user_soldier_id: us.id, tool_id: ut&.id)
+          WorkingRecord.create(user_soldier_id: us.id, user_tool: ut&.id)
           ut&.using
           success
         end
