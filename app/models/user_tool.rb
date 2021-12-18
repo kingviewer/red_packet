4702f9592c291d855fdd401b0f1f0c154a4f88cb
孟庆巍 <keven@lonzh.com>
2021-12-18 17:42:27 +0800

    init

--- app/models/user_tool.rb
+++ app/models/user_tool.rb
@@ -7,6 +7,7 @@ class UserTool < ApplicationRecord
   def self.increase_for_user(user_id, tool_id, amount)
     user_tool = UserTool.find_by(user_id: user_id, tool_id: tool_id) ||
       UserTool.create(user_id: user_id, tool_id: tool_id)
-    UserTool.where(id: user_tool.id).update_all(['amount = amount + ?, state = ?', amount, :available])
+    UserTool.where(id: user_tool.id).update_all(['amount = amount + ?', amount])
+    user_tool.available!
   end
 end
